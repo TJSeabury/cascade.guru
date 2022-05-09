aff50758a2928c15528ff4ebf18793c7b99092ab
TJSeabury <tylerseabury@gmail.com>
2022-05-09 15:48:19 -0400

    fixed absolute url regex

--- lib/url.ts
+++ lib/url.ts
@@ -1,6 +1,6 @@
 export const hasProtocol = (uri: string): boolean => /^https?:\/\//gm.test(uri);
 
-export const linkIsAbsolute = (uri: string): boolean => /^https?:\/\/(?:\w+?\.){1,9}\w+\/?|^(?:\w+?\.){0,9}\w+\/?/gm.test(uri);
+export const linkIsAbsolute = (uri: string): boolean => /^https?:\/\/(?:\w+?\.){1,9}\w+\/?|^(?:\w+?\.){1,9}\w+\/?/gm.test(uri);
 
 export const linkIsRelative = (uri: string): boolean => !linkIsAbsolute(uri);
 
@@ -11,8 +11,8 @@ export const linkIsRelative = (uri: string): boolean => !linkIsAbsolute(uri);
  * @returns boolean
  */
 export const linkIsLocal = (uri: string, domain: string): boolean => {
-    if (linkIsRelative(uri)) return true;
-    return new RegExp(`^${domain}`).test(uri);
+  if (linkIsRelative(uri)) return true;
+  return new RegExp(`^${domain}`).test(uri);
 };
 
 export const linkIsExternal = (uri: string, domain: string): boolean => !linkIsLocal(uri, domain);
@@ -24,23 +24,23 @@ export const linkIsExternal = (uri: string, domain: string): boolean => !linkIsL
  * @returns boolean
  */
 export const isUrl = (u: string): boolean => {
-    try {
-        if (linkIsAbsolute(u)) {
-            new URL(u);
-            return true;
-        }
-    } catch (error) {
-        return false;
+  try {
+    if (linkIsAbsolute(u)) {
+      new URL(u);
+      return true;
     }
+  } catch (error) {
     return false;
+  }
+  return false;
 };
 
 export const isNotUrl = (u: string): boolean => !isUrl(u);
 
 export const makeLinkAbsolute = (link: string, domain: string): string => {
-    if (linkIsAbsolute(link)) return link;
-    if (! /^\//.test(link)) link = '/' + link;
-    return domain + link;
+  if (linkIsAbsolute(link)) return link;
+  if (! /^\//.test(link)) link = '/' + link;
+  return domain + link;
 };
 
 export const trimSlashes = (url: string): string => url.replace(/^\/{2,}|\/+$/g, '');
@@ -51,30 +51,30 @@ export const trimSlashes = (url: string): string => url.replace(/^\/{2,}|\/+$/g,
  * @returns {object} URL
  */
 export const urlResolver = (uri: string, hostname: string | null = null): [string, any] => {
-    try {
-        uri = trimSlashes(uri);
-        if (!hasProtocol(uri)) {
-            if (linkIsAbsolute(uri)) {
-                return [new URL(`https://${uri}`).href, null];
-            } else {
-                if (!hostname) throw new Error(`${uri} requires a hostname to resolve.`);
-                return [new URL(`https://${hostname}/${uri}`).href, null];
-            }
-        } else {
-            return [new URL(uri).href, null];
-        }
-    } catch (err) {
-        return [uri, err];
+  try {
+    uri = trimSlashes(uri);
+    if (!hasProtocol(uri)) {
+      if (linkIsAbsolute(uri)) {
+        return [new URL(`https://${uri}`).href, null];
+      } else {
+        if (!hostname) throw new Error(`${uri} requires a hostname to resolve.`);
+        return [new URL(`https://${hostname}/${uri}`).href, null];
+      }
+    } else {
+      return [new URL(uri).href, null];
     }
+  } catch (err) {
+    return [uri, err];
+  }
 };
 
 export const extractFileUri = (url: string | null): [string | null, Error | null] => {
-    if (!url) throw new Error('URL string must be provided!');
-    const reg = /[\w\:\/\.-]+\/([\w\.-]+?)\.css/gm;
-    const fileUri = reg.exec(url)?.[1];
-    if (!fileUri) return [
-        null,
-        new Error('Failed to extract URI.')
-    ];
-    return [fileUri, null];
+  if (!url) throw new Error('URL string must be provided!');
+  const reg = /[\w\:\/\.-]+\/([\w\.-]+?)\.css/gm;
+  const fileUri = reg.exec(url)?.[1];
+  if (!fileUri) return [
+    null,
+    new Error('Failed to extract URI.')
+  ];
+  return [fileUri, null];
 };
