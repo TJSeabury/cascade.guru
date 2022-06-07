3bd2e58b5f5a3b60eaa5e404b00a6460db3babd7
TJSeabury <tylerseabury@gmail.com>
2022-06-06 22:03:51 -0400

    method should be GET, formatting

--- pages/api/admin/listusers.ts
+++ pages/api/admin/listusers.ts
@@ -3,24 +3,24 @@ import { getSession } from "next-auth/react"
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const session = await getSession({ req })
-        if (session) {
-            const userList = await prisma.user
-                .findMany()
-                .catch((e) => {
-                    throw e
-                })
-                .finally(async () => {
-                    await prisma.$disconnect()
-                })
-            res.status(200).json(userList)
-        } else {
-            res.status(401).json({
-                error: "You are not authorized to access this endpoint.",
-            })
-        }
+  if (req.method === 'GET') {
+    const session = await getSession({ req })
+    if (session) {
+      const userList = await prisma.user
+        .findMany()
+        .catch((e) => {
+          throw e
+        })
+        .finally(async () => {
+          await prisma.$disconnect()
+        })
+      res.status(200).json(userList)
     } else {
-        res.status(404).json('Endpoint not found.');
+      res.status(401).json({
+        error: "You are not authorized to access this endpoint.",
+      })
     }
+  } else {
+    res.status(404).json('Endpoint not found.');
+  }
 }
