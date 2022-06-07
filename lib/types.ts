2ee73eedb3dc88c043326ada1ebb4c03e1239bb1
TJSeabury <tylerseabury@gmail.com>
2022-06-06 22:05:00 -0400

    updated types to match schema and added PlanTypes

--- lib/types.ts
+++ lib/types.ts
@@ -1,12 +1,24 @@
 export enum userRoles {
-    admin = "admin",
-    user = "user"
+  admin = "admin",
+  user = "user"
+}
+
+export enum planTypes {
+  agency = "agency",
+  team = "team",
+  single = "single"
 }
 
 export type User = {
-    email: string,
-    password: string,
-    forename: string | undefined,
-    surname: string | undefined,
-    userRole: userRoles
+  email: string,
+  password: string,
+  forename: string | undefined,
+  surname: string | undefined,
+  userRole: userRoles,
+  numberOfProperties: number | undefined,
+  maxProperties: number | undefined,
+  planType: planTypes,
+  billable: boolean | undefined,
+  discountPercent: number | undefined
 };
+
